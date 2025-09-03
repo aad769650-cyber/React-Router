c449c692ee69c5316bd81447fe876994996da6b2
Mahar Abdul Qudoos <aad769650@gmail.com>
2025-09-03 15:45:17 +0500

    About sec added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,37 @@
+import { NavLink } from "react-router"
+
 export const About=()=>{
     return(
         <>
-        <h1>Hello I am About Page</h1>
+      
+<div className="home-container">
+
+<div className="home-content">
+<div className="home-area">
+  <div>
+  Explore The Latest in Movie Industries
+</div>
+<h1>Unlimited Movie,TVs Shows & More</h1>
+
+<p>Discover the top Best Movies and Deamas with catchy subtitle like your Ultimate Guide to Must-Watch Content</p>
+<NavLink to="/movie">
+<button className="home-btn">Explore Now</button></NavLink>
+
+</div>
+
+
+</div>
+<div className="home-image">
+<img src="/movies.png" alt="" />
+</div>
+
+
+</div>
+
+
+
+
+
         </>
     )
 }
